1ac64916a31b4e5c77e5118073e9b54cd67d4a67
Odd Stråbø <oddstr13@openshell.no>
2013-08-31 01:10:04 +0200

    Reindent invscan.lua to 4-spaces

--- minecraft/invscan.lua
+++ minecraft/invscan.lua
@@ -5,35 +5,35 @@ baseURL = "http://sp.svennp.com/invscan/php/post.php?name="
 
 rs.setOutput("top", false)
 while true do
-	targets = proxSensor.getTargets()
-	for name, information in pairs(targets) do
-		if information.RawName == "net.minecraft.entity.player.EntityPlayerMP" then
-			print("Player ["..name.."] found!")
-			local details = proxSensor.getTargetDetails(name)
-			local postInfo = ""
-			print("Creating info string!")
-			for slot, stack in pairs(details.Inventory) do
-				if stack.RawName then
-					postInfo = postInfo.."item["..slot.."][rawName]="..stack.RawName.."&"
-					postInfo = postInfo.."item["..slot.."][name]="..stack.Name.."&"
-					postInfo = postInfo.."item["..slot.."][size]="..stack.Size.."&"
-				end
-			end
-			-- Add more info?
-			print("Starting http-request!")
-			http.request(baseURL..name, postInfo)
-			completed = false
-			repeat
-				local event, url, sourceText = os.pullEvent()
-				if event == "http_success" then
-					print("Success!")
-					completed = true
-				elseif event == "http_failure" then
-					print("Failure!")
-					completed = true
-				end
-			until completed
-		end
-	end
-	sleep(4)
+    targets = proxSensor.getTargets()
+    for name, information in pairs(targets) do
+        if information.RawName == "net.minecraft.entity.player.EntityPlayerMP" then
+            print("Player ["..name.."] found!")
+            local details = proxSensor.getTargetDetails(name)
+            local postInfo = ""
+            print("Creating info string!")
+            for slot, stack in pairs(details.Inventory) do
+                if stack.RawName then
+                    postInfo = postInfo.."item["..slot.."][rawName]="..stack.RawName.."&"
+                    postInfo = postInfo.."item["..slot.."][name]="..stack.Name.."&"
+                    postInfo = postInfo.."item["..slot.."][size]="..stack.Size.."&"
+                end
+            end
+            -- Add more info?
+            print("Starting http-request!")
+            http.request(baseURL..name, postInfo)
+            completed = false
+            repeat
+                local event, url, sourceText = os.pullEvent()
+                if event == "http_success" then
+                    print("Success!")
+                    completed = true
+                elseif event == "http_failure" then
+                    print("Failure!")
+                    completed = true
+                end
+            until completed
+        end
+    end
+    sleep(4)
 end
